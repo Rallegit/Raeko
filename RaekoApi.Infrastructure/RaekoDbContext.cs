a8decbc263032b3ed384edd7659ea098894dacdd
Rasmus <ralle.sodergren@gmail.com>
2025-10-09 23:53:53 +0200

    Initial commit

---
+++ RaekoApi.Infrastructure/RaekoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RaekoApi.Infrastructure
+{
+    public class RaekoDbContext : DbContext
+    {
+        public RaekoDbContext(DbContextOptions<RaekoDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ RaekoApi/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+using RaekoApi.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<RaekoDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+ 
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
